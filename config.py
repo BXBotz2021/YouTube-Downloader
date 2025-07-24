70a36e287502b3f8d143c71e2e1ef7591d2da585
MUFAZ VK <93363575+BXBotz2021@users.noreply.github.com>
2025-07-24 07:44:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ))
-    API_HASH = os.getenv("API_HASH", '')
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '')
+    API_ID = int(os.getenv("API_ID", 7813390))
+    API_HASH = os.getenv("API_HASH", '1faadd9cc60374bca1e88c2f44e3ee2f')
+    BOT_TOKEN = os.getenv("BOT_TOKEN", '8132280304:AAEAotAxpqUbh1Lqd0HaLx43TR__g3T2Vr4')
